372a37e62ec04ab0f23a4dbf0dd9f4530acfc510
yxyz <2298096865@qq.com>
2025-11-22 23:07:31 +0800

    第二次修正

--- Assignment-6-VCall/VCall.cpp
+++ Assignment-6-VCall/VCall.cpp
@@ -1,5 +1,5 @@
 /**
- * Andersen.cpp
+ * VCall.cpp
  * @author kisslune
  */
 
@@ -8,11 +8,11 @@
 using namespace llvm;
 using namespace std;
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     auto moduleNameVec =
-            OptionBase::parseOptions(argc, argv, "Whole Program Points-to Analysis",
-                                     "[options] <input-bitcode...>");
+        OptionBase::parseOptions(argc, argv, "Whole Program Points-to Analysis",
+                                 "[options] <input-bitcode...>");
 
     SVF::LLVMModuleSet::buildSVFModule(moduleNameVec);
 
@@ -24,190 +24,176 @@ int main(int argc, char** argv)
     Andersen andersen(consg);
     auto cg = pag->getCallGraph();
 
-    // TODO: complete the following two methods
+    // TODO: 完成以下两个方法
     andersen.runPointerAnalysis();
     andersen.updateCallGraph(cg);
 
     cg->dump();
     SVF::LLVMModuleSet::releaseLLVMModuleSet();
-	return 0;
+    return 0;
 }
 
 
 void Andersen::runPointerAnalysis()
 {
-    // TODO: complete this method. Point-to set and worklist are defined in A5Header.h
-    //  The implementation of constraint graph is provided in the SVF library
-    
+    // TODO: 完成此方法。点集和工作列表定义在A6Header.h中
+    //  约束图的实现在SVF库中提供
     WorkList<unsigned> wl;
-    auto pag = SVF::PAG::getPAG();
-    
-    // Initialize worklist with Addr edges: p = &o -> o ∈ pts(p)
-    for (auto it = consg->begin(); it != consg->end(); ++it)
-    {
-        auto node = it->second;
-        for (auto edgeIt = node->getOutEdges().begin(); 
-             edgeIt != node->getOutEdges().end(); ++edgeIt)
+
+    // 辅助函数：检查从src到dst的Copy边是否存在
+    auto hasCopyEdge = [this](unsigned srcId, unsigned dstId) -> bool {
+        SVF::ConstraintNode *srcNode = consg->getConstraintNode(srcId);
+        if (!srcNode) return false;
+        
+        for (auto copyEdge : srcNode->getCopyOutEdges())
         {
-            auto edge = *edgeIt;
-            if (edge->getEdgeKind() == SVF::ConstraintEdge::Addr)
-            {
-                unsigned src = edge->getSrcID();  // o (the object)
-                unsigned dst = edge->getDstID();  // p (the pointer)
-                // p = &o -> o ∈ pts(p), so dst points to src
-                pts[dst].insert(src);
-                wl.push(dst);
-            }
+            if (copyEdge->getDstID() == dstId)
+                return true;
+        }
+        return false;
+    };
+
+    // 辅助函数：检查从src到dst的Copy边是否存在（通过入边检查）
+    auto hasCopyEdgeIn = [this](unsigned srcId, unsigned dstId) -> bool {
+        SVF::ConstraintNode *dstNode = consg->getConstraintNode(dstId);
+        if (!dstNode) return false;
+        
+        for (auto copyEdge : dstNode->getCopyInEdges())
+        {
+            if (copyEdge->getSrcID() == srcId)
+                return true;
+        }
+        return false;
+    };
+
+    // 初始化：处理所有Addr边，建立初始点集
+    for (auto iter = consg->begin(); iter != consg->end(); ++iter)
+    {
+        SVF::ConstraintNode *constraintNode = iter->second;
+        
+        for (auto addrEdge : constraintNode->getAddrInEdges())
+        {
+            SVF::AddrCGEdge *edge = SVF::SVFUtil::dyn_cast<SVF::AddrCGEdge>(addrEdge);
+            if (!edge) continue;
+            
+            unsigned objId = edge->getSrcID();
+            unsigned ptrId = edge->getDstID();
+            
+            pts[ptrId].insert(objId);
+            wl.push(ptrId);
         }
     }
-    
-    // Process worklist
+
+    // 迭代处理工作列表
     while (!wl.empty())
     {
-        unsigned src = wl.pop();
-        
-        // Process Copy edges: q = p -> pts(p) ⊆ pts(q)
-        auto node = consg->getCGNode(src);
-        if (node)
+        unsigned ptrId = wl.pop();
+        SVF::ConstraintNode *ptrNode = consg->getConstraintNode(ptrId);
+        if (!ptrNode) continue;
+
+        // 处理Store边：*ptr = src，需要添加 src --Copy--> obj 边
+        for (auto storeEdge : ptrNode->getStoreInEdges())
         {
-            for (auto edgeIt = node->getOutEdges().begin(); 
-                 edgeIt != node->getOutEdges().end(); ++edgeIt)
+            SVF::StoreCGEdge *edge = SVF::SVFUtil::dyn_cast<SVF::StoreCGEdge>(storeEdge);
+            if (!edge) continue;
+            
+            unsigned srcId = edge->getSrcID();
+            
+            // 对于ptr点集中的每个对象，检查并添加Copy边
+            for (auto objId : pts[ptrId])
             {
-                auto edge = *edgeIt;
-                unsigned dst = edge->getDstID();
+                if (hasCopyEdge(srcId, objId))
+                    continue;
                 
-                if (edge->getEdgeKind() == SVF::ConstraintEdge::Copy)
-                {
-                    // pts(src) ⊆ pts(dst)
-                    bool changed = false;
-                    for (auto pointee : pts[src])
-                    {
-                        if (pts[dst].insert(pointee).second)
-                            changed = true;
-                    }
-                    if (changed)
-                        wl.push(dst);
-                }
-                else if (edge->getEdgeKind() == SVF::ConstraintEdge::Load)
-                {
-                    // q = *p -> ∀o ∈ pts(p): pts(o) ⊆ pts(q)
-                    bool changed = false;
-                    for (auto pointee : pts[src])
-                    {
-                        // pts(pointee) ⊆ pts(dst)
-                        for (auto pointee2 : pts[pointee])
-                        {
-                            if (pts[dst].insert(pointee2).second)
-                                changed = true;
-                        }
-                    }
-                    if (changed)
-                        wl.push(dst);
-                }
+                consg->addCopyCGEdge(srcId, objId);
+                wl.push(srcId);
+            }
+        }
+
+        // 处理Load边：dst = *ptr，需要添加 obj --Copy--> dst 边
+        for (auto loadEdge : ptrNode->getLoadOutEdges())
+        {
+            SVF::LoadCGEdge *edge = SVF::SVFUtil::dyn_cast<SVF::LoadCGEdge>(loadEdge);
+            if (!edge) continue;
+            
+            unsigned dstId = edge->getDstID();
+            
+            // 对于ptr点集中的每个对象，检查并添加Copy边
+            for (auto objId : pts[ptrId])
+            {
+                if (hasCopyEdgeIn(objId, dstId))
+                    continue;
+                
+                consg->addCopyCGEdge(objId, dstId);
+                wl.push(objId);
+            }
+        }
+
+        // 处理Copy边：target = ptr，传播点集
+        for (auto copyEdge : ptrNode->getCopyOutEdges())
+        {
+            SVF::CopyCGEdge *edge = SVF::SVFUtil::dyn_cast<SVF::CopyCGEdge>(copyEdge);
+            if (!edge) continue;
+            
+            unsigned targetId = edge->getDstID();
+            size_t prevSize = pts[targetId].size();
+            
+            pts[targetId].insert(pts[ptrId].begin(), pts[ptrId].end());
+            
+            if (pts[targetId].size() > prevSize)
+                wl.push(targetId);
+        }
+
+        // 处理Gep边：target = ptr->field，处理字段访问
+        for (auto gepEdge : ptrNode->getGepOutEdges())
+        {
+            SVF::GepCGEdge *edge = SVF::SVFUtil::dyn_cast<SVF::GepCGEdge>(gepEdge);
+            if (!edge) continue;
+            
+            unsigned targetId = edge->getDstID();
+            size_t prevSize = pts[targetId].size();
+            
+            for (auto objId : pts[ptrId])
+            {
+                unsigned fieldObjId = consg->getGepObjVar(objId, edge);
+                pts[targetId].insert(fieldObjId);
             }
             
-            // Process Store edges: *p = q -> ∀o ∈ pts(p): pts(q) ⊆ pts(o)
-            for (auto edgeIt = node->getInEdges().begin(); 
-                 edgeIt != node->getInEdges().end(); ++edgeIt)
-            {
-                auto edge = *edgeIt;
-                if (edge->getEdgeKind() == SVF::ConstraintEdge::Store)
-                {
-                    unsigned storeSrc = edge->getSrcID();
-                    // ∀o ∈ pts(src): pts(storeSrc) ⊆ pts(o)
-                    bool changed = false;
-                    for (auto pointee : pts[src])
-                    {
-                        for (auto pointee2 : pts[storeSrc])
-                        {
-                            if (pts[pointee].insert(pointee2).second)
-                            {
-                                changed = true;
-                            }
-                        }
-                    }
-                    if (changed)
-                    {
-                        for (auto pointee : pts[src])
-                            wl.push(pointee);
-                    }
-                }
-            }
+            if (pts[targetId].size() > prevSize)
+                wl.push(targetId);
         }
     }
 }
 
 
-void Andersen::updateCallGraph(SVF::CallGraph* cg)
+void Andersen::updateCallGraph(SVF::CallGraph *cg)
 {
-    // TODO: complete this method.
-    //  The implementation of call graph is provided in the SVF library
-    
-    auto pag = SVF::PAG::getPAG();
-    auto icfg = pag->getICFG();
-    
-    // Iterate through all ICFG nodes to find indirect call sites
-    for (auto it = icfg->begin(); it != icfg->end(); ++it)
+    // TODO: 完成此方法
+    //  调用图的实现在SVF库中提供
+    const auto &allIndirectCalls = consg->getIndirectCallsites();
+
+    for (const auto &callsiteInfo : allIndirectCalls)
     {
-        SVF::ICFGNode* node = it->second;
-        
-        // Check if this is an indirect call site
-        if (auto* callNode = SVF::SVFUtil::dyn_cast<SVF::CallICFGNode>(node))
+        SVF::CallBlockNode *callsite = callsiteInfo.first;
+        unsigned funcPtrId = callsiteInfo.second;
+
+        // 检查函数指针的点集是否存在
+        auto it = pts.find(funcPtrId);
+        if (it == pts.end())
+            continue;
+
+        const SVF::Function *caller = callsite->getCaller();
+        const auto &funcPtrPointsTo = it->second;
+
+        // 遍历函数指针可能指向的所有对象
+        for (unsigned potentialFuncId : funcPtrPointsTo)
         {
-            if (callNode->isIndirectCall())
-            {
-                // Get the function pointer ID from the call site
-                // According to Lec06.md: "ConstraintGraph :: Corresponding ICFG Node  The ID of the function pointer"
-                SVF::CallBlockNode* callBlock = callNode->getCallBlockNode();
-                if (callBlock)
-                {
-                    // Get the call instruction from call block
-                    const llvm::Instruction* callInst = callBlock->getCallSite();
-                    if (callInst)
-                    {
-                        // Get the function pointer PAG node
-                        SVF::PAGNode* funPtrNode = pag->getCallSiteFunPtr(callInst);
-                        if (funPtrNode)
-                        {
-                            unsigned funPtrID = funPtrNode->getId();
-                            
-                            // Get the caller function
-                            const SVF::Function* caller = callNode->getFun();
-                            
-                            // Find all functions that the function pointer may point to
-                            if (pts.find(funPtrID) != pts.end())
-                            {
-                                for (auto funObjID : pts[funPtrID])
-                                {
-                                    // Get the PAG node for the function object
-                                    SVF::PAGNode* funObjNode = pag->getGNode(funObjID);
-                                    if (funObjNode)
-                                    {
-                                        // Get the function from the node's value
-                                        const SVF::Function* callee = nullptr;
-                                        if (funObjNode->hasValue())
-                                        {
-                                            callee = pag->getFunction(funObjNode->getValue());
-                                        }
-                                        
-                                        if (callee)
-                                        {
-                                            // Add edge to call graph
-                                            SVF::PTACallGraphNode* callerNode = cg->getCallGraphNode(caller);
-                                            SVF::PTACallGraphNode* calleeNode = cg->getCallGraphNode(callee);
-                                            
-                                            if (callerNode && calleeNode)
-                                            {
-                                                cg->addIndirectCallGraphEdge(callerNode, calleeNode, callNode);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            // 跳过非函数对象
+            if (!consg->isFunction(potentialFuncId))
+                continue;
+            
+            const SVF::Function *callee = consg->getFunction(potentialFuncId);
+            cg->addIndirectCallGraphEdge(callsite, caller, callee);
         }
     }
 }
